0870d34bb00b1dd643a122460a7fb07471d35195
寂鸿 <jihong.sj@alibaba-inc.com>
2017-10-29 19:07:46 +0800

    修改参数和hook文件

--- webhook.js
+++ webhook.js
@@ -1,9 +1,21 @@
 var http = require('http')
 var createHandler = require('github-webhook-handler')
 var handler = createHandler({ path: '/webhook', secret: 'shijin821' })
+const spawn = require('child_process').spawn;
 
 http.createServer(function (req, res) {
-    console.log(req);
+    const ls =spawn('sh', ['./auto_build.sh']);
+    ls.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    });
+
+    ls.stderr.on('data', (data) => {
+        console.log(`stderr: ${data}`);
+    });
+
+    ls.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
     handler(req, res, function (err) {
         res.statusCode = 404
         res.end('no such location')
@@ -18,6 +30,7 @@ handler.on('push', function (event) {
     console.log('Received a push event for %s to %s',
         event.payload.repository.name,
         event.payload.ref)
+    spawn('sh', ['./auto_build']);
 })
 
 handler.on('issues', function (event) {
