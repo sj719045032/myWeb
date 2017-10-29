b6184a7a22e73bc8f02fa27fdb351a32ba3ea792
寂鸿 <jihong.sj@alibaba-inc.com>
2017-10-29 19:55:05 +0800

    数据修改

--- app/controller/home.js
+++ app/controller/home.js
@@ -3,7 +3,7 @@
 module.exports = app => {
   class HomeController extends app.Controller {
     * index() {
-      this.ctx.body = '呵呵哒';
+      this.ctx.body = '嘻嘻嘻嘻';
     }
   }
   return HomeController;
