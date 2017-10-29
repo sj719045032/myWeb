48af240fabe1e9d0b8908a2891d00d69fc734a93
寂鸿 <jihong.sj@alibaba-inc.com>
2017-10-29 19:50:06 +0800

    数据修改

--- app/controller/home.js
+++ app/controller/home.js
@@ -3,7 +3,7 @@
 module.exports = app => {
   class HomeController extends app.Controller {
     * index() {
-      this.ctx.body = '啦啦啦啦啦啦啦啦啦啦啦啦啦啦';
+      this.ctx.body = '呵呵哒';
     }
   }
   return HomeController;
--- webhook.js
+++ webhook.js
@@ -1,35 +1,24 @@
 var http = require('http')
 var createHandler = require('github-webhook-handler')
-var handler = createHandler({ path: '/webhook', secret: 'shijin821' })
+var handler = createHandler({path: '/webhook', secret: 'shijin821'})
 const spawn = require('child_process').spawn;
 
-http.createServer(function (req, res) {
-    handler(req, res, function (err) {
+http.createServer((req, res) => {
+    handler(req, res, err => {
         res.statusCode = 404
         res.end('no such location')
     })
 }).listen(7777)
 
-handler.on('error', function (err) {
+handler.on('error', err => {
     console.error('Error:', err.message)
 })
 
-handler.on('push', function (event) {
+handler.on('push', event => {
     console.log('Received a push event for %s to %s',
         event.payload.repository.name,
         event.payload.ref)
-    const sh= spawn('sh', ['./auto_build.sh']);
-    sh.stdout.on('data', (data) => {
-        console.log(`stdout: ${data}`);
-    });
-
-    sh.stderr.on('data', (data) => {
-        console.log(`stderr: ${data}`);
-    });
-
-    sh.on('close', (code) => {
-        console.log(`child process exited with code ${code}`);
-    });
+    spawn('sh', ['./auto_build.sh']);
 })
 
 
